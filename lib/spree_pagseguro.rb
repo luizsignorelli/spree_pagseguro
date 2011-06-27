9166c9c19bfa3bb38bd61ff760bd2cbfb2f8c6c0
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2011-06-27 16:18:34 -0300

    removed unused things; bumped version

--- lib/spree_pagseguro.rb
+++ lib/spree_pagseguro.rb
@@ -7,16 +7,10 @@ module SpreePagseguro
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      #workaround for https://github.com/Shopify/active_merchant/issuesearch?state=open&q=paypal#issue/43
-      require 'active_merchant'
-      ActiveMerchant::Billing::PaypalExpressGateway
-
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
 
-      BillingIntegration::PaypalExpress.register
-      BillingIntegration::PaypalExpressUk.register
       PaymentMethod::Pagseguro.register
     end
 
